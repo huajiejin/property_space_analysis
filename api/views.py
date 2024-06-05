b833e06027056e19179c6ba9395158c0d2d923d2
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2024-06-04 20:49:55 -0400

    feat: update PropertySpaceView to retrieve property space data

--- api/views.py
+++ api/views.py
@@ -1,12 +1,31 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.views import View
-from .models import PropertySpace
-from django.core import serializers
+from .models import PropertySpace, UnitSpace, MeterData
 
 class PropertySpaceView(View):
     def get(self, request, *args, **kwargs):
+        data = []
         if kwargs.get('id'):
-            space = serializers.serialize('json', [PropertySpace.objects.get(pk=kwargs['id'])])
+            spaces = [PropertySpace.objects.get(pk=kwargs['id'])]
         else:
-            space = serializers.serialize('json', PropertySpace.objects.all())
-        return HttpResponse(space, content_type='application/json')
+            spaces = PropertySpace.objects.all()
+
+        for space in spaces:
+            units = UnitSpace.objects.filter(property_space=space)
+            meters = MeterData.objects.filter(unit_space__in=units)
+            data.append({
+                "name": space.name,
+                "address": {
+                    "street": space.address.street,
+                    "city": space.address.city,
+                    "state": space.address.state,
+                    "country": space.address.country,
+                    "postal_code": space.address.postal_code,
+                },
+                "number_of_units": units.count(),
+                "total_area": sum(unit.area for unit in units),
+                "total_consumption": sum(meter.measurement_reading for meter in meters),
+                "consumption_unit": "kWh" if meters else None,
+            })
+
+        return JsonResponse(data, safe=False)
