36011c8882bbc8f8c65a47c4c67be46eb19ea13e
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2024-06-05 11:59:49 -0400

    chore: remove dead code PropertySpaceView and PropertySpaceViewTestCase

--- property_space_analysis/urls.py
+++ property_space_analysis/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 from api.endpoints.api_v1 import api_v1
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include("api.urls")),
     path("api/v1/", api_v1.urls),
 ]
